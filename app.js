078cb11098952366975143ba6da112c66130a508
Marek <marekstankiewicz6@wp.pl>
2023-06-20 12:20:57 +0200

    finish build app

--- app.js
+++ app.js
@@ -1,20 +1,54 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello Worls');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Set static path
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/users', (req, res) => {
+  let users = [
+    {
+      first_name: "John",
+      last_name: "Doe",
+      age: 30,
+      gender: "male"
+    },
+    {
+      first_name: "Tom",
+      last_name: "Jackson",
+      age: 23,
+      gender: "male"
+    },
+    {
+      first_name: "Tracy",
+      last_name: "Smith",
+      age: 38,
+      gender: "female"
+    }
+  ]
+  res.json(users);
+});
+
+app.get('/download', (req, res) => {
+  res.download(path.join(__dirname, '/downloads/pdf.pdf'));
 });
 
 app.get('/about', (req, res) => {
-  res.send('<h1>About</h1>');
+  res.redirect('/about.html');
 });
 
-app.get('/users/:name', (req, res) => {
-  let user = req.params.name;
-  res.send('<h1>'+user+'</h1>');
+app.post('/subscribe', (req, res) => {
+  let name = req.body.name;
+  let email = req.body.email;
+
+  console.log(name + ' has subscribed ' + email);
 });
 
-app.listen(3000, function() {
+app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
